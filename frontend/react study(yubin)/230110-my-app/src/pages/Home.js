99f452e7d274b3b15bc3d295e565e9624ff6df35
조유빈 <yubeen00@naver.com>
2023-01-20 15:42:29 +0900

    react study, new mockup

---
+++ frontend/react study(yubin)/230110-my-app/src/pages/Home.js
@@ -0,0 +1,19 @@
+// 함수형 컴포넌트 방식(최근 추세)
+import React from "react";
+
+function Home() {
+    return <h1>Home 화면입니다.</h1>;
+}
+
+export default Home;
+
+// // 클래스형 컴포넌트 방식
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render() {
+//         return <h1>Home 화면입니다.</h1>
+//     }
+// }
+
+// export default Home;
