0a5b05d8a63ff5d5365aae0f71a3096fe677b248
박상현 <pas4584@naver.com>
2023-01-20 15:31:52 +0900

    crudtest

---
+++ backend/crudtest-sanghyun/src/main/java/com/ssafy/crudtest/CrudtestApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.crudtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudtestApplication.class, args);
+    }
+
+}
