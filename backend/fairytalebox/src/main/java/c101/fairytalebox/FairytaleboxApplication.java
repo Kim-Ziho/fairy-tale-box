292fce9f94fcece4055f7c2929b1a2d1f26f79b2
조승현 <sansusjc3@naver.com>
2023-01-27 17:06:28 +0900

    start backend project

---
+++ backend/fairytalebox/src/main/java/c101/fairytalebox/FairytaleboxApplication.java
@@ -0,0 +1,13 @@
+package c101.fairytalebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairytaleboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairytaleboxApplication.class, args);
+	}
+
+}
