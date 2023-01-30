9bb8a106d4a8decf278d4cb6959039d469d2523e
조승현 <sansusjc3@naver.com>
2023-01-30 17:52:30 +0900

    [BE] feat : Create_entity_version_word | 단어 엔티티 작성중

---
+++ backend/fairytalebox/src/main/java/c101/fairytalebox/controller/MemberController.java
@@ -0,0 +1,9 @@
+package c101.fairytalebox.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+}
---
+++ backend/fairytalebox/src/main/java/c101/fairytalebox/domain/Member.java
@@ -0,0 +1,28 @@
+package c101.fairytalebox.domain;
+
+import lombok.Getter;
+import lombok.NonNull;
+
+import javax.persistence.Column;
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id@GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String nickname;
+
+    @OneToOne(mappedBy = "member", fetch = FetchType.LAZY)
+    private RaspberrySerial raspberrySerial;
+
+}
