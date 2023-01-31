527ae95157922a980a68c95eaddf035d8bea6c6a
조유빈 <yubeen00@naver.com>
2023-01-31 15:54:40 +0900

    [FE] style : home and modal css

--- frontend/my-app/src/pages/Home.js
+++ frontend/my-app/src/pages/Home.js
@@ -11,11 +11,9 @@ import Back from "./Back";
 import "./Home.css";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
-  color: theme.palette.text.secondary,
 }));
 
 const Home = (props) => {
@@ -35,8 +33,26 @@ const Home = (props) => {
       <Grid container spacing={4}>
         <Grid item xs={4}>
           <Item>
-            <button onClick={openModal}>
-              <img src="img/sunandmoon_select1.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select1.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              모달 바꿀거임
+            </Modal>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select1.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
             </button>
             <Modal open={modalOpen} close={closeModal} header="해님 달님">
               {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
@@ -53,42 +69,156 @@ const Home = (props) => {
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select2.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select1.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select3.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select2.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select1.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select2.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select2.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select2.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select3.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select3.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select1.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select3.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <img src="img/sunandmoon_select2.png" id="img1"></img>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-            <img src="img/sunandmoon_select3.png" id="img1"></img>
+            <button onClick={openModal} className="selectBtn">
+              <img
+                src="img/sunandmoon_select3.png"
+                className="selectImg"
+                alt="sun and moon"
+              ></img>
+            </button>
+            <Modal open={modalOpen} close={closeModal} header="해님 달님">
+              {/* Modal.js <main> {props.children} </main>에 내용이 입력된다. */}
+              해와 달이 된 오누이 그 신화...!
+              <Grid container>
+                <Grid item xs={12}>
+                  <Link to="/scene1">
+                    <button>하러가기</button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Modal>
           </Item>
         </Grid>
       </Grid>
--- frontend/my-app/src/pages/Modaldal.js
+++ frontend/my-app/src/pages/Modaldal.js
@@ -1,16 +1,16 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Button from "@mui/material/Button";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
   boxShadow: 24,
   pt: 2,
   px: 4,
@@ -28,6 +28,7 @@ function ChildModal() {
 
   return (
     <React.Fragment>
+      
       <Button onClick={handleOpen}>이전</Button>
       <Button onClick={handleOpen}>시작하기</Button>
       <Modal
