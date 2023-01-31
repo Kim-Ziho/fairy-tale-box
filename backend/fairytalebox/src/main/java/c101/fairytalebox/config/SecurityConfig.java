b01eafc459aca829ad3ca61654035e0256d52fd5
조승현 <sansusjc3@naver.com>
2023-01-31 15:45:43 +0900

    [BE] feat : study_jwt

--- backend/fairytalebox/src/main/java/c101/fairytalebox/config/SecurityConfig.java
+++ backend/fairytalebox/src/main/java/c101/fairytalebox/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurationAdapter {
-}
+//@Configuration
+//@EnableWebSecurity
+////public class SecurityConfig extends WebSecurityConfigurationAdapter {
+////}
