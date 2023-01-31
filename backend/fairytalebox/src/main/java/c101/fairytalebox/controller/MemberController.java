449d2dc8755f156ceabf9b8109fe0bf12233da38
조승현 <sansusjc3@naver.com>
2023-01-31 15:43:46 +0900

    [BE] feat : study_jwt

--- backend/fairytalebox/src/main/java/c101/fairytalebox/controller/MemberController.java
+++ backend/fairytalebox/src/main/java/c101/fairytalebox/controller/MemberController.java
@@ -1,9 +1,17 @@
 package c101.fairytalebox.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class MemberController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
+    }
 }
