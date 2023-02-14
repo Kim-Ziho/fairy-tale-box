5884d1b1bb3005b26df10c437b50eca11f599d4a
조유빈 <yubeen00@naver.com>
2023-02-14 11:48:03 +0900

    [FE] feat : axios error solved

--- frontend/my-app/src/pages/Qr.js
+++ frontend/my-app/src/pages/Qr.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 import "./Qr.css";
 
 const Qr = () => {
-
   return (
     <div className="qrContainer">
       <h1 className="qrMainText txt">🔒 로그인하기 🔑</h1>
       <div className="qrText txt">
         로그인하시려면 휴대폰으로 아래 QR코드를 스캔해주세요!
       </div>
-      <img src="http://i8c101.p.ssafy.io/login" className="qrimg" alt="#"></img>
+      <img
+        src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=http://i8c101.p.ssafy.io/login"
+        className="qrimg"
+        alt="#"
+      ></img>
       <div className="qrFooter">
         <Link to="/">
           <button className="qrButton txt">
