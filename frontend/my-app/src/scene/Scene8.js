89fdcfaa75bd14b03f3f98124364b66bea91bd8d
유승규 <shark0872@naver.com>
2023-02-16 09:41:40 +0900

    [FE] style : scene

--- frontend/my-app/src/scene/Scene8.js
+++ frontend/my-app/src/scene/Scene8.js
@@ -43,7 +43,7 @@ const Scene8 = () => {
     document.addEventListener("mousemove", (e) => { // mousemove이벤트를 이용해 움
   
       // 마우스의 좌표는 clientX와 clientY를 이용해 알수 있다. -> 브라우저 window의 좌표값 위치를 전달한다.
-  
+      
       // pageX, pageY와는 다름.
       const mouseX = e.clientX;
   
