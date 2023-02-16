1b89e3cad1202a697609a4c4a3c7f4affc2016fa
조승현 <sansusjc3@naver.com>
2023-02-16 11:29:02 +0900

    [BE] hotfix : signup

--- backend/fairytalebox/src/main/java/c101/fairytalebox/service/MemberServiceImpl.java
+++ backend/fairytalebox/src/main/java/c101/fairytalebox/service/MemberServiceImpl.java
@@ -52,8 +52,13 @@ public class MemberServiceImpl implements MemberService {
         if (!request.getEmail().equals("admin@naver.com")) {
             RaspberrySerial raspberrySerial = raspberrySerialRepository.findBySerialNum(request.getSerialNum())
                     .orElseThrow(() -> new IllegalArgumentException("기기 정보가 올바르지 않습니다."));
-
+            MemberRaspberry memberRaspberry = memberRaspberryRepository.findByRaspberrySerial(raspberrySerial)
+                    .orElse(null);
+            if (memberRaspberry != null) {
+                throw new Exception("이미 등록된 기기입니다.");
+            }
         }
+
 
         try {
             Member member = memberRepository.save(request.toEntity());
@@ -84,6 +89,8 @@ public class MemberServiceImpl implements MemberService {
 
         RaspberrySerial raspberrySerial = raspberrySerialRepository.findBySerialNum(member.getSerialNum())
                 .orElse(null);
+
+
 
         // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
         // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
